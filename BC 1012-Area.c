cc7b65c02140d43356a799db4003803de1ed856a
sajjad-jisun <sajjad.jisun@gmail.com>
2024-08-15 23:41:37 +0600

    H C

---
+++ BC 1012-Area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main(){
+    double pi=3.14159, A,B,C;
+    scanf("%lf %lf %lf",&A,&B,&C);
+
+    printf("TRIANGULO: %.3lf\n",A*C);
+    printf("CIRCULO: %.3lf\n",pi*C*C);
+    printf("TRAPEZIO: %.3lf\n",.5*(A+B)*C);
+    printf("QUADRADO: %.3lf\n",B*B);
+    printf("RETANGULO: %.3lf\n",A*B);
+    
+    return 0;
+ }
