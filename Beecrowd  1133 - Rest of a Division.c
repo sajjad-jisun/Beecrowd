e04eb49f23bd6acae84223a699da9ff8aa1d96ee
Sajjad Jisun <sajjad.jisun@gmail.com>
2024-09-04 14:49:43 +0600

    Fun

---
+++ Beecrowd  1133 - Rest of a Division.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int X,Y;
+    scanf("%d %d",&X,&Y);
+    if(X>Y){
+        int temp=X;
+        X=Y;
+        Y=temp;
+    }
+    for(int i=X+1; i<Y; i++){
+        if(i%5==2 || i%5==3) printf("%d\n",i);
+    }
+    return 0;
+}
+ 
